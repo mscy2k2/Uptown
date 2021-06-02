5f960bb8c4fe61defaf52059d25d41a77fbf9448
Eduwem Akpan <akpane1@ferris.edu>
2021-06-01 23:21:13 -0400

    program simulates multiple coin tosses

---
+++ src/CoinTest.java
@@ -0,0 +1,25 @@
+
+public class CoinTest {
+
+	public static void main(String[] args) {
+	   Coin dime = new Coin();
+	   int numHeads = 0;
+	   int numTails = 0;
+	   int numTosses = 10;
+	   
+	   System.out.println("Initial coin side facing up: " + dime.getStatus() + "\n");
+	   
+	   for (int currentToss = 1; currentToss <= numTosses; currentToss++) {
+		   dime.toss();
+		   if (dime.getStatus() == "heads") {
+			   numHeads += 1;
+		   }
+		   else {
+			   numTails += 1;
+		   }
+		   System.out.println("Toss " + ": " + dime.getStatus());
+	   }
+//System.out.println("\nOut of " + numTosses + " tosses,there were " + numHeads + " heads and " + numTails + " tails.");
+	}
+
+}
