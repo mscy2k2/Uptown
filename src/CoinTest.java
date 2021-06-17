31d24baf76f25584df5324a499a4f0b397dfb2a7
Eduwem Akpan <akpane1@ferris.edu>
2021-06-16 22:22:20 -0400

    user predicts heads or tails for coin toss

--- src/CoinTest.java
+++ src/CoinTest.java
@@ -1,15 +1,29 @@
-
+import java.util.Scanner;
 public class CoinTest {
 
 	public static void main(String[] args) {
 	   Coin dime = new Coin();
 	   int numHeads = 0;
 	   int numTails = 0;
-	   int numTosses = 10;
-	   
+	   //int numTosses = 10;
+	   String guess;
+	   Scanner scnr = new Scanner(System.in);
+	   System.out.print("Enter prediction(heads or tails): ");
+		  guess = scnr.next();
+		  
 	   System.out.println("Initial coin side facing up: " + dime.getStatus() + "\n");
 	   
-	   for (int currentToss = 1; currentToss <= numTosses; currentToss++) {
+	   if (dime.getStatus().equals("heads") && guess.equals("heads")){
+		   System.out.println("Your guess is as right as good");
+	   }
+	   else if (dime.getStatus().equals("tailss") && guess.equals("tails")){
+		   System.out.println("Your guess is as right as good");
+	   }
+	   else
+		   System.out.println("Your guess is wrong");
+	   
+	   
+	   /*for (int currentToss = 1; currentToss <= numTosses; currentToss++) {
 		   dime.toss();
 		   if (dime.getStatus() == "heads") {
 			   numHeads += 1;
@@ -20,7 +34,7 @@ public class CoinTest {
 		   System.out.println("Toss " + ": " + dime.getStatus());
 	   }
        
-	   System.out.println("\nOut of " + numTosses + " tosses,there were " + numHeads + " heads and " + numTails + " tails.");
+	   System.out.println("\nOut of " + numTosses + " tosses,there were " + numHeads + " heads and " + numTails + " tails.");*/
 	}
 
 }
